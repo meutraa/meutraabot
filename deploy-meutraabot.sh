3aae9eb01175204e0a07a804aed5426321d49776
meutraa <paul@lost.host>
2021-01-19 15:01:22 +0000

    Remove binary

--- deploy-meutraabot.sh
+++ deploy-meutraabot.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
-go build ./cmd/meutraabot || exit
-
-rm meutraabot
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-extldflags -static" ./cmd/meutraabot
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-extldflags -static" ./cmd/meutraabot || exit
 rsync -aP meutraabot 192.168.1.102:
 ssh root@192.168.1.102 /usr/bin/env sh << EOF
         systemctl stop meutraabot && \
-        cp /home/paul/meutraabot /etc/nixos/bin/meutraabot/ && \
+        cp meutraabot /etc/nixos/bin/meutraabot/ && \
         systemctl start meutraabot && \
         journalctl -xefu meutraabot
 EOF
